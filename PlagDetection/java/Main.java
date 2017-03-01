d68eb6735baf3387d005f89ee38571eed8507fa2
veyseler <veyseler510@hotmail.com>
2017-03-01 09:26:40 +0300

    asdfads

--- PlagDetection/java/Main.java
+++ PlagDetection/java/Main.java
@@ -1,27 +1,31 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 
 
 public class Main {
 
-	public static void print (ArrayList<ArrayList <Integer>> windows)
+	public static void print  (ArrayList<ArrayList <Integer>> windows) throws Exception
 	{
 		int rows=windows.size();
 		int cols=windows.get(0).size();
+		FileWriter writer=new FileWriter ("001-Short.txt");
+		BufferedWriter bw=new BufferedWriter (writer);
 		
 		
 		for (int i = 0; i < rows; i++) {
-			System.out.print("(");
+			bw.write("(");
 			for (int j = 0; j < cols; j++) {
 				if(j!= cols-1)
-					System.out.print(windows.get(i).get(j) + ", " );
+					bw.write(windows.get(i).get(j) + ", " );
 				else
-					System.out.print(windows.get(i).get(j)  );
+					bw.write(windows.get(i).get(j)  );
 			}
 		
-			System.out.print(")\n");
+			bw.write(")\n");
 		}
 		
 	}
@@ -60,7 +64,7 @@ public class Main {
 		for(int i=0;!isFinished && i<=k_grams.size();i++)
 		{
 			ArrayList<Integer> window =new ArrayList<>();
-			for (int j = i; !isFinished && j <= i+10; j++) {
+			for (int j = i; !isFinished && j < i+10; j++) {
 				
 			
 				int hashed=(hashTrials.hash(k_grams.get(j), prevVal, prevFirstVal));
